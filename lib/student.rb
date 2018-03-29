c12a91560021becd7bf43d6c757c8f9d7db74895
Alexander Spanos <alexisspa9@gmail.com>
2018-03-29 08:25:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,11 @@ class Student
     @id = id
   end
   def self.create_table
-    sql = << SQL
+    sql = << SQL "CREATE TABLE IF NOT EXISTS student(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );"
 
   end
 
